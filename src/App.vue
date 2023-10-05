686f68201cfe56ec8187fe256c16ea9a0c9a1f0b
guylain.thomas <guylain.thomas@epitech.eu>
2023-10-05 16:08:29 +0200

    add the init of the project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <nav id="nav">
+        <img id="photo" src="src/assets/Pablo_Ibar_(cropped).png" />
+    </nav>
+</template>
+
+<style lang="scss">
+    #nav {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: #000;
+    }
+
+    #photo {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+</style>
