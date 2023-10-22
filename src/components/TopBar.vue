f47d6b19cde61eecd87bc221ce3822f85999e6cb
Benjamin <benjamin.colimard@epitech.eu>
2023-10-22 13:11:27 +0200

    add: basic topbar with title

---
+++ src/components/TopBar.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup name="TopBar">
+import { defineProps } from 'vue';
+const props = defineProps({
+    title: { type: String, required: true },
+})
+</script>
+
+<template>
+    <div>
+        <h3>{{ props.title }}</h3>
+    </div>
+</template>
+
+<style scoped>
+div {
+    background-color: gray;
+    height: 40px;
+    top: 0px;
+}
+h3 {
+    color: white;
+}
+</style>
