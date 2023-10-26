f4029b3aff1a41c068c5e9fc4b21ca866da7c172
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-10-26 15:02:12 +0200

    feat(RoomButton.vue): add component

---
+++ app/src/components/RoomButton.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+    import { computed, defineProps } from 'vue';
+
+    const props = defineProps({
+      roomName: String,
+      lampsLit: Boolean,
+    });
+
+    const buttonClass = computed(() => {
+      return {
+        'lit': props.lampsLit,
+        'not-lit': !props.lampsLit,
+      };
+    });
+
+</script>
+
+<template>
+    <button :class="['button', buttonClass]">
+        {{ roomName }}
+    </button>
+</template>
+
+<style scoped>
+.lit {
+  background-color: green;
+  color: white;
+}
+
+.not-lit {
+  background-color: red;
+  color: white;
+}
+
+.button {
+  border: none;
+  border-radius: 5px;
+  padding: 50px;
+  margin: 20px;
+  font-size: 1.5em;
+}
+
+</style>
