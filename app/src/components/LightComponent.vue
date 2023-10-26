86154d86ea164a17346a3aabd1ec9f85c229cffd
Jibea <guylain.thomas@epitech.eu>
2023-10-26 16:38:17 +0200

    feat(LightComponent) : add the smaller piece of objects management page with the component light

---
+++ app/src/components/LightComponent.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+    import { defineProps } from 'vue';
+    import { defineComponent } from 'vue';
+    import OnOff from '@/components/OnOff.vue';
+
+
+    defineProps({
+        msg: String,
+    });
+
+    defineComponent({
+        name: 'LightComponent',
+        components: {
+            OnOff,
+        },
+    });
+
+</script>
+
+<template>
+    <div class="lightComponent">
+        <div class="element">
+            <h2>{{ msg }}</h2>
+            <OnOff />
+        </div>
+    </div>
+</template>
+
+
+<style scoped lang="scss">
+    .lightComponent {
+        .element {
+            display : flex;
+            background-color: #42b983;
+            color: #fff;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: auto;
+            width: auto;
+            padding: 8px;
+        }
+    }
+</style>
