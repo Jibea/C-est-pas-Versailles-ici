6c6e8c2c381fe68a7032b844bf0b39250025f3c6
Benjamin <benjamin.colimard@epitech.eu>
2023-11-02 16:41:41 +0100

    feat: button for edit mode + input for new room

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -4,19 +4,40 @@
     import TopBar from '@/components/TopBar.vue';
     import RoomButton from '@/components/RoomButton.vue';
     import draggable from 'vuedraggable'
+
 const rooms = ref([
     {name: 'Poudlard', lights: false},
     {name: 'Vogons', lights: false},
     {name: 'Gotham', lights: true},
 ]);
+let isInput = ref(false);
+let message = ref("");
+
 onMounted(() => {
+    // TODO get tous les groupes des rooms dans l'api
     const savedList = localStorage.getItem("draggable-list");
     if (savedList) {
         rooms.value = JSON.parse(savedList);
     }
 });
-function onSave() {
+const onSave = () => {
+    // TODO save in api new group of room
     localStorage.setItem("draggable-list", JSON.stringify(rooms.value));
+}
+
+const addRoom = () => {
+    const newRoom = message.value
+    const newItem = { name: newRoom, lights: false };
+    rooms.value.push(newItem);
+}
+const changeMod = () => {
+    if (isInput.value) {
+        isInput.value = false
+    } else {
+        isInput.value = true
+        message.value = ""
+        onSave()
+    }
 }
 </script>
 
@@ -27,5 +48,10 @@ function onSave() {
             <RoomButton  :roomName="room.name" :lampsLit="room.lights" ></RoomButton>
         </template>
     </draggable>
-    <button @click="onSave">Save</button>
+    <button @click="changeMod">Edit mode</button>
+    <div v-if="isInput">
+        <input v-model="message" placeholder="edit me"/>
+        <button @click="addRoom">Add a room</button>
+        <button @click="onSave">Save</button>
+    </div>
 </template>
