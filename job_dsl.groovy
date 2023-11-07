db92001251506e11fd75e11000ee66698ba0ad38
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-11-07 11:13:56 +0100

    feat: update groovy file

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,60 +1,17 @@
-folder('Tools') {
-    displayName('Tools')
-    description('Folder for miscellaneous tools.')
-}
-
-freeStyleJob('Tools/clone-repository') {
-    parameters {
-        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
-    }
-    wrappers {
-        preBuildCleanup {
-            deleteDirectories()
+pipeline {
+    agent any
+    stages {
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    sh 'docker build -t c-est-pas-versailles-ici_vue-js-app:latest .'
+                }
+            }
         }
     }
-    steps {
-        shell('git clone $GIT_REPOSITORY_URL')
-    }
-}
-
-freeStyleJob('Tools/SEED') {
-    parameters {
-        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
-        stringParam('DISPLAY_NAME', '', 'Display name for the job')
-        stringParam('SSH_KEY_CREDENTIALS', '', 'SSH Key Credentials ID in Jenkins')
-    }
-    steps {
-        dsl {
-            text('''
-                freeStyleJob(DISPLAY_NAME) {
-                    wrappers {
-                        preBuildCleanup {
-                            deleteDirectories()
-                        }
-                    }
-                    scm {
-                        git {
-                            remote {
-                                url('git@github.com:${GITHUB_NAME}.git')
-                                credentials(SSH_KEY_CREDENTIALS)
-                            }
-                            branches('*/main')
-                        }
-                    }
-                    triggers {
-                        pollSCM {
-                            scmpoll_spec('* * * * *')
-                            ignorePostCommitHooks(false)
-                        }
-                    }
-                    steps {
-                        shell('make fclean')
-                        shell('make')
-                        shell('make tests_run')
-                        shell('make clean')
-                    }
-                }
-            ''')
+    post {
+        success {
+            echo 'Docker image built successfully!'
         }
     }
 }
