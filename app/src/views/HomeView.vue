a3ba5f4b650344418c1523230cba7145f68aaf49
Benjicatch <91670393+Benjicatch@users.noreply.github.com>
2023-11-08 13:05:16 +0100

    feat(23 > main): view rooms (#40)
    * feat: basic draggable room component
    
    * feat: add parameters to put in roomButton
    
    * feat: save in local order draggable
    
    * feat: button to save order list
    
    * feat: button for edit mode + input for new room
    
    * feat: drag and drop only on edit

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,61 @@
 
 <script lang="ts" setup name="HomeView">
+    import { ref, onMounted } from "vue";
     import TopBar from '@/components/TopBar.vue';
+    import RoomButton from '@/components/RoomButton.vue';
+    import draggable from 'vuedraggable'
+
+const rooms = ref([
+    {name: 'Poudlard', lights: false},
+    {name: 'Vogons', lights: false},
+    {name: 'Gotham', lights: true},
+]);
+let isEdit = ref(false);
+let message = ref("");
+
+onMounted(() => {
+    // TODO get tous les groupes des rooms dans l'api
+    const savedList = localStorage.getItem("draggable-list");
+    if (savedList) {
+        rooms.value = JSON.parse(savedList);
+    }
+});
+const onSave = () => {
+    // TODO save in api new group of room
+    localStorage.setItem("draggable-list", JSON.stringify(rooms.value));
+}
+
+const addRoom = () => {
+    const newRoom = message.value
+    const newItem = { name: newRoom, lights: false };
+    rooms.value.push(newItem);
+}
+const changeMod = () => {
+    if (isEdit.value) {
+        isEdit.value = false
+        onSave()
+    } else {
+        isEdit.value = true
+        message.value = ""
+    }
+}
+const onDragStart = (event) => {
+    if (!isEdit.value) {
+        event.preventDefault();
+    }
+}
 </script>
 
 <template>
     <TopBar title="Home"/>
-    <h1>This is the Home page</h1>
+    <draggable v-model="rooms" :animation="300" @dragstart="onDragStart">
+        <template #item="{ element: room }">
+            <RoomButton  :roomName="room.name" :lampsLit="room.lights" ></RoomButton>
+        </template>
+    </draggable>
+    <button @click="changeMod">Edit mode</button>
+    <div v-if="isEdit">
+        <input v-model="message" placeholder="Name of new room"/>
+        <button @click="addRoom">Add a room</button>
+    </div>
 </template>
