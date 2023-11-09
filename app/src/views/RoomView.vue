52250ff7a8feadf187e5c7299844441568aa431f
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-11-09 16:16:04 +0100

    feat: adapt RoomView.vue for the new types

--- app/src/views/RoomView.vue
+++ app/src/views/RoomView.vue
@@ -2,12 +2,11 @@
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import axios from 'axios';
-import { Group } from '@/types/Groups';
+import { GroupsResponse } from '@/types/GroupsResponse';
 
 const roomName = ref('');
 const gatewayIP = '127.17.0.2'
-
-const groups = ref<Group[]>([]);
+const groups = ref<GroupsResponse>({});
 
 onMounted(() => {
     const route = useRoute();
@@ -20,7 +19,7 @@ const getGroups = async () => {
     try {
         const response = await axios.get(`http://${gatewayIP}/api/${APIKey}/groups`);
         groups.value = response.data;
-        console.log(response.data);
+        console.log('Groups data: ', groups.value);
     } catch (error) {
         console.error('Error API: ', error);
     }
@@ -29,16 +28,16 @@ const getGroups = async () => {
 </script>
 
 <template>
-    <div>
-        <h1>Room View</h1>
-        <p>Room name: {{ roomName }}</p>
-        <h2>Groups</h2>
-        <ul>
-            <li v-for="group in groups" :key="group.id" class="group-item">
-                {{ group.name }}
-            </li>
-        </ul>
-    </div>
+  <div>
+    <h1>Room View</h1>
+    <p>Room name: {{ roomName }}</p>
+    <h2>Groups</h2>
+    <ul>
+      <li v-for="(group, groupId) in groups" :key="groupId" class="group-item">
+        {{ group.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
