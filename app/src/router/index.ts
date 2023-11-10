f0601ded9c3a22ae53fb8cd96e8c0a0a86721435
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-11-10 11:39:21 +0100

    feat: add new route on index.ts

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -2,15 +2,13 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RoomView from '../views/RoomView.vue'
 import AboutView from '../views/AboutView.vue'
+import GroupView from '../views/GroupView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'home', component: HomeView },
-  {
-    path: '/about',
-    name: 'about',
-    component: AboutView
-  },
-  { path: '/room/:roomName', name: 'room', component: RoomView}
+  { path: '/about', name: 'about', component: AboutView },
+  { path: '/room/:roomName', name: 'room', component: RoomView},
+  { path: '/room/groups/:groupId', name: 'group', component: GroupView}
 ]
 
 const router = createRouter({
