7d034734fee4a1806be8746b7aeb77885a2b098c
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-11-10 18:43:22 +0100

    fix: room params + rooms names router

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -7,8 +7,8 @@ import GroupView from '../views/GroupView.vue'
 const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/about', name: 'about', component: AboutView },
-  { path: '/room/:roomName', name: 'room', component: RoomView},
-  { path: '/room/groups/:groupId', name: 'group', component: GroupView}
+  { path: '/room/:roomName', name: 'roomRoute', component: RoomView},
+  { path: '/room/:roomName/:groupId', name: 'groupRoute', component: GroupView}
 ]
 
 const router = createRouter({
