65a654c050c91e0965d1149494580e6cc116fdea
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-11-24 16:19:55 +0100

    feat: add arrow icon for library

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faPen, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faPen);
+library.add(faPen, faArrowLeft);
 createApp(App)
 .use(router)
 .component("font-awesome-icon", FontAwesomeIcon)
