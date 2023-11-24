4403c74a5d93346ca19404e147ae6e4ce3a21696
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-11-24 16:25:37 +0100

    feat: add arrow to comeback to the last page on the topbar

--- app/src/components/TopBar.vue
+++ app/src/components/TopBar.vue
@@ -1,32 +1,51 @@
 <script lang="ts" setup name="TopBar">
     import { defineProps } from 'vue';
     import BurgerComponent from '@/components/BurgerComponent.vue';
+    import { useRouter } from 'vue-router';
 
 const props = defineProps({
     title: { type: String, required: true },
 })
+
+const router = useRouter();
+
+const goBack = () => {
+    router.go(-1);
+}
+
 </script>
 
 <template>
-    <div>
-        <BurgerComponent />
-        <h3>{{ props.title }}</h3>
+    <div class="top-bar">
+      <BurgerComponent />
+      <font-awesome-icon
+        :icon="['fas', 'arrow-left']"
+        class="arrow-icon"
+        @click="goBack"
+      />
+      <h3 class="title">{{ props.title }}</h3>
     </div>
-</template>
-
-<style scoped>
-div {
+  </template>
+  
+  <style scoped>
+  .top-bar {
     background-color: gray;
-    flex-direction: column;
+    display: flex;
     align-items: center;
-    justify-content: center;
-    text-align: center;
-}
-h3 {
+    padding: 40px;
+  }
+  
+  .title {
     color: white;
-    justify-content: center;
-    text-align: center;
-    padding-top: 2%;
-    padding-bottom: 2%;
-}
-</style>
+    font-size: 24px;
+    margin: 0 auto;
+  }
+  
+  .arrow-icon {
+    color: white;
+    cursor: pointer;
+    font-size: 24px;
+    position: relative;
+    left: 70px;
+  }
+  </style>
