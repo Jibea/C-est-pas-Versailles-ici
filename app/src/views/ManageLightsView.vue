407282dbc3bb7c913119def817b8d30b18cb73b8
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-11-24 10:55:42 +0100

    feat: managaelights fanlly works

--- app/src/views/ManageLightsView.vue
+++ app/src/views/ManageLightsView.vue
@@ -10,7 +10,8 @@ const gatewayIP = '127.17.0.2'
 const APIKey = "key";
 const router = useRouter();
 const group = ref<GroupAttributes>();
-const lights = ref<Light[]>([]);
+const lights = ref<string[]>([]);
+const detailedLights = ref<Record<string, Light>>({});
 const groupId = decodeURIComponent(router.currentRoute.value.params.groupId);
 const allLights = ref<LightsApiResponse>();
 const renameDialogOpen = ref(false);
@@ -25,120 +26,118 @@ onMounted(() => {
 });
 
 const getGroup = async () => {
-  try {
-    const response = await axios.get(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`);
-    group.value = response.data;
-    console.log('Group data: ', response.data);
-    const lightsInGroupIds = group.value?.lights || [];
-    await Promise.all(lightsInGroupIds.map((lightId: string) => getLight(lightId)));
-  } catch (error) {
-    console.error('Error API: ', error);
-  }
+    try {
+        const response = await axios.get(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`);
+        group.value = response.data;
+        console.log('Group data: ', response.data);
+        const lightsInGroupIds = group.value?.lights || [];
+        await Promise.all(lightsInGroupIds.map((lightId: string) => getLight(lightId)));        
+        lights.value = lightsInGroupIds;
+    } catch (error) {
+        console.error('Error API: ', error);
+    }
 };
 
 const getLight = async (lightId: string) => {
     try {
         const response = await axios.get(`http://${gatewayIP}/api/${APIKey}/lights/${lightId}`);
         const lightData = response.data;
-        lights.value.push(lightData);
+        detailedLights.value = {
+            ...detailedLights.value,
+            [lightId]: lightData,
+        };
         console.log('Light data: ', lightData);
     } catch (error) {
         console.error('Error API: ', error);
     }
-}
-
-const removeLight = async (lightId: number) => {
-  try {
-    const currentGroup = await axios.get(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`);
-    const groupAttributes = currentGroup.data;
-
-    if (groupAttributes.lights) {
-      const stringLightId = String(lightId);
-      groupAttributes.lights = groupAttributes.lights.filter(id => id !== stringLightId);
-      const response = await axios.put(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`, groupAttributes);
-      console.log('Light removed from group. API Response:', response.data);
-      await getGroup();
-      await getAllLights().then((response) => {
-        allLights.value = response;
-      });
-
-    } else {
-      console.warn('No lights in the group to remove.');
-    }
-  } catch (error) {
-    console.error('Error API: ', error);
-  }
 };
 
+const removeLight = async (lightId: string) => {
+    try {
+        const currentGroup = await axios.get(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`);
+        const groupAttributes = currentGroup.data;
+
+        if (groupAttributes.lights) {
+            groupAttributes.lights = groupAttributes.lights.filter((id: string) => id !== lightId);
+            const response = await axios.put(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`, groupAttributes);
+            console.log('Light removed from group. API Response:', response.data);
+            await getGroup();
+            await getAllLights().then((response) => {
+                allLights.value = response;
+            });
+        } else {
+            console.warn('No lights in the group to remove.');
+        }
+    } catch (error) {
+        console.error('Error API: ', error);
+    }
+};
 
 const getAllLights = async (): Promise<LightsApiResponse> => {
-  try {
-    const allLightsResponse = await axios.get(`http://${gatewayIP}/api/${APIKey}/lights`);
-    const lightsInGroupIds = group.value?.lights || [];
-    const lightsNotInGroup = Object.fromEntries(
-      Object.entries(allLightsResponse.data)
-        .filter(([lightId]) => !lightsInGroupIds.includes(lightId))
-        .map(([lightId, lightData]) => [lightId, { ...lightData, id: lightId }])
-    );
+    try {
+        const allLightsResponse = await axios.get(`http://${gatewayIP}/api/${APIKey}/lights`);
+        const lightsInGroupIds = group.value?.lights || [];
+        const lightsNotInGroup = Object.fromEntries(
+            Object.entries(allLightsResponse.data)
+                .filter(([lightId]) => !lightsInGroupIds.includes(lightId))
+                .map(([lightId, lightData]) => [lightId, { ...lightData, id: lightId }])
+        );
 
-    return lightsNotInGroup;
-  } catch (error) {
-    console.error('Error API: ', error);
-  }
-};
-
-const addLightToGroup = async (lightId: number) => {
-  try {
-    const currentGroup = await axios.get(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`);
-    const groupAttributes = currentGroup.data;
-
-    if (!groupAttributes.lights) {
-      groupAttributes.lights = [];
+        return lightsNotInGroup;
+    } catch (error) {
+        console.error('Error API: ', error);
     }
-    groupAttributes.lights.push(String(lightId));
-    const response = await axios.put(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`, groupAttributes);
-    console.log('Light added to group: ', response.data);
-    await getGroup();
-    await getAllLights().then((response) => {
-      allLights.value = response;
-    });
-
-  } catch (error) {
-    console.error('Error adding light to group: ', error.response || error.message || error);
-  }
 };
 
-const renameLight = async (lightId: number, newName: string) => {
-  console.log('Renaming light: ', lightId, newName);
-  try {
-    const response = await axios.put(
-      `http://${gatewayIP}/api/${APIKey}/lights/${lightId}`,
-      { name: newName }
-    );
-    console.log('Light renamed: ', response.data);
+const addLightToGroup = async (lightId: string) => {
+    try {
+        const currentGroup = await axios.get(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`);
+        const groupAttributes = currentGroup.data;
 
-    renameDialogOpen.value = false;
-    newLightName.value = '';
-    await getAllLights().then((response) => {
-        allLights.value = response;
-    });
-
-  } catch (error) {
-    console.error('Error API: ', error);
-  }
+        if (!groupAttributes.lights) {
+            groupAttributes.lights = [];
+        }
+        groupAttributes.lights.push(lightId);
+        const response = await axios.put(`http://${gatewayIP}/api/${APIKey}/groups/${groupId}`, groupAttributes);
+        console.log('Light added to group: ', response.data);
+        await getGroup();
+        await getAllLights().then((response) => {
+            allLights.value = response;
+        });
+    } catch (error) {
+        console.error('Error adding light to group: ', error.response || error.message || error);
+    }
 };
 
-const openRenameDialog = (lightId: number) => {
-  console.log('Opening rename dialog for light: ', lightId);
-  renameDialogOpen.value = true;
-  lightIdToRename.value = lightId;
+const renameLight = async (lightId: string, newName: string) => {
+    console.log('Renaming light: ', lightId, newName);
+    try {
+        const response = await axios.put(
+            `http://${gatewayIP}/api/${APIKey}/lights/${lightId}`,
+            { name: newName }
+        );
+        console.log('Light renamed: ', response.data);
+
+        renameDialogOpen.value = false;
+        newLightName.value = '';
+        await getAllLights().then((response) => {
+            allLights.value = response;
+        });
+    } catch (error) {
+        console.error('Error API: ', error);
+    }
+};
+
+const openRenameDialog = (lightId: string) => {
+    console.log('Opening rename dialog for light: ', lightId);
+    renameDialogOpen.value = true;
+    lightIdToRename.value = lightId;
 };
 
 const cancelRename = () => {
-  renameDialogOpen.value = false;
-  newLightName.value = '';
-  newLightName.value = '';
-}
+    renameDialogOpen.value = false;
+    newLightName.value = '';
+};
 
 </script>
 
@@ -148,59 +147,60 @@ const cancelRename = () => {
 
     <h1>Lights in the Group</h1>
     <ul class="light-list">
-      <li v-for="(light, lightId) in lights" :key="lightId" class="light-item">
-        <div class="light-info">
-          <p class="light-name">{{ light.name }}</p>
-          <p class="light-state">{{ light.state.on ? 'On' : 'Off' }}</p>
-          <p class="light-manufacturer">Manufacturer: {{ light.manufacturername }}</p>
-          <p class="light-manufacturer">Model: {{ light.modelid }}</p>
-          <p class="light-manufacturer">Unique ID: {{ light.uniqueid }}</p>
-        </div>
+        <li v-for="lightId in lights" :key="lightId" class="light-item">
+            <div class="light-info">
+                <p class="light-name">{{ detailedLights[lightId]?.name || 'Loading...' }}</p>
+                <p class="light-state">{{ detailedLights[lightId]?.state && detailedLights[lightId]?.state.on ? 'On' : 'Off' }}</p>
+                <p class="light-manufacturer">Manufacturer: {{ detailedLights[lightId]?.manufacturername || 'Loading...' }}</p>
+                <p class="light-manufacturer">Model: {{ detailedLights[lightId]?.modelid || 'Loading...' }}</p>
+                <p class="light-manufacturer">Unique ID: {{ detailedLights[lightId]?.uniqueid || 'Loading...' }}</p>
+            </div>
 
-        {{ lightId }}
+            {{ lightId }}
 
-        <button @click="removeLight(lightId)">Remove</button>
-        <button @click="openRenameDialog(lightId)">Rename</button>
-      
-        <div v-if="renameDialogOpen" class="rename-dialog">
-          <label for="newLightName">Enter new name:</label>
-          <input v-model="newLightName" type="text" id="newLightName" class="rename-input"/>
-          <div class="button-container">
-            <button @click="renameLight(lightId, newLightName)">Confirm</button>
-            <button @click="cancelRename">Cancel</button>
-          </div>
-        </div>
-      </li> 
+            <button @click="removeLight(lightId)">Remove</button>
+            <button @click="openRenameDialog(lightId)">Rename</button>
+
+            <div v-if="renameDialogOpen" class="rename-dialog">
+                <label for="newLightName">Enter new name:</label>
+                <input v-model="newLightName" type="text" id="newLightName" class="rename-input"/>
+                <div class="button-container">
+                    <button @click="renameLight(lightId, newLightName)">Confirm</button>
+                    <button @click="cancelRename">Cancel</button>
+                </div>
+            </div>
+        </li>
     </ul>
 
     <h2>All Lights</h2>
     <ul class="light-list">
-      <li v-for="(light, lightId) in allLights" :key="lightId" class="light-item">
-        <div class="light-info">
-          <p class="light-name">{{ light.name }}</p>
-          <p class="light-state">{{ light.state.on ? 'On' : 'Off' }}</p>
-          <p class="light-manufacturer">Manufacturer: {{ light.manufacturername }}</p>
-          <p class="light-manufacturer">Model: {{ light.modelid }}</p>
-          <p class="light-manufacturer">Unique ID: {{ light.uniqueid }}</p>
-        </div>
+        <li v-for="(light, lightId) in allLights" :key="lightId" class="light-item">
+            <div class="light-info">
+                <p class="light-name">{{ light.name }}</p>
+                <p class="light-state">{{ light.state.on ? 'On' : 'Off' }}</p>
+                <p class="light-manufacturer">Manufacturer: {{ light.manufacturername }}</p>
+                <p class="light-manufacturer">Model: {{ light.modelid }}</p>
+                <p class="light-manufacturer">Unique ID: {{ light.uniqueid }}</p>
+            </div>
 
-        {{ lightId }}
+            {{ lightId }}
 
-        <button @click="addLightToGroup(lightId)">Add to Group</button>
-        <button @click="openRenameDialog(lightId)">Rename</button>
-      
-        <div v-if="renameDialogOpen" class="rename-dialog">
-          <label for="newLightName">Enter new name:</label>
-          <input v-model="newLightName" type="text" id="newLightName" class="rename-input"/>
-          <div class="button-container">
-            <button @click="renameLight(lightId, newLightName)">Confirm</button>
-            <button @click="cancelRename">Cancel</button>
-          </div>
-        </div>
-      </li>
+            <button @click="addLightToGroup(lightId)">Add to Group</button>
+            <button @click="openRenameDialog(lightId)">Rename</button>
+
+            <div v-if="renameDialogOpen" class="rename-dialog">
+                <label for="newLightName">Enter new name:</label>
+                <input v-model="newLightName" type="text" id="newLightName" class="rename-input"/>
+                <div class="button-container">
+                    <button @click="renameLight(lightId, newLightName)">Confirm</button>
+                    <button @click="cancelRename">Cancel</button>
+                </div>
+            </div>
+        </li>
     </ul>
   </div>
 </template>
+
 
 <style scoped>
 
