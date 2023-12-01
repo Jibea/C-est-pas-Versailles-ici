07a1ccbf947b1b0dec3853f961eabee5df2f2e3f
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-01 12:31:22 +0100

    feat: filter the groups for the specific group

--- app/src/views/ScheduleView.vue
+++ app/src/views/ScheduleView.vue
@@ -5,16 +5,26 @@ import axios from 'axios';
 import { Schedule } from '@/types/Schedule';
 
 const schedules = ref<Record<string, Schedule>>({});
+const currentGroupId = ref<string>('');
 
 onMounted(() => {
     const route = useRoute();
+    currentGroupId.value = route.params.groupId;
     getSchedule();
 });
 
 const getSchedule = async () => {
     try {
         const response = await axios.get(`http://${process.env.VUE_APP_GATEWAY_IP}/api/${process.env.VUE_APP_API_KEY}/schedules`);
-        schedules.value = response.data;
+        
+        const filteredSchedules = Object.fromEntries(
+            Object.entries(response.data).filter(([key, schedule]: [string, unknown]) => {
+                const typeSchedule = schedule as Schedule;
+                return typeSchedule.command.address.includes(`/groups/${currentGroupId.value}/`);
+            })
+        );
+
+        schedules.value = filteredSchedules;
     } catch (error) {
         console.error(error);
     }
@@ -28,13 +38,9 @@ const getSchedule = async () => {
 
     <ul>
       <li v-for="schedule in schedules" :key="schedule.id">
-        {{ schedule.name  }} - {{ schedule.time  }}
+        {{ schedule.name }} - {{ schedule.time }}
       </li>
     </ul>
 
   </div>
 </template>
-
-<style scoped>
-
-</style>
