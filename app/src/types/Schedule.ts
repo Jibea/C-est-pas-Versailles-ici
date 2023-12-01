53a686877994b1d52dba5ebc6374091ce7cb00b1
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-01 12:04:31 +0100

    feat: add Schedule type

---
+++ app/src/types/Schedule.ts
@@ -0,0 +1,13 @@
+export interface Schedule {
+    autodelete: boolean;
+    command: {
+      address: string;
+      method: string;
+      body?: Record<string, any>;
+    };
+    description: string;
+    etag: string;
+    name: string;
+    status: string;
+    time: string;
+}
