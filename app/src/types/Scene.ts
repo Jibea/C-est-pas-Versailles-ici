7bc732f5b09e149df53de41eab69cba7f2a2361f
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-07 17:23:05 +0100

    feat: add commentary for Scene.ts

--- app/src/types/Scene.ts
+++ app/src/types/Scene.ts
@@ -2,6 +2,7 @@ interface SceneInfo {
     lights: string[];
     name: string;
     transitiontime: number;
+    id: string; // my creation caus i need it for schedule
 }
 
 export interface Scene {
