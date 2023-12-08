96451777aea18ace65d64475861532f72ee52af4
Jibea <guylain.thomas@epitech.eu>
2023-12-08 16:43:39 +0100

    refactor(switch on off): delete some useless lines and chnage the condition of switchOnOff for group to on => any_on

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -7,7 +7,6 @@
     import draggable from 'vuedraggable'
     import axios from "axios";
     import { Group } from "@/types/Group";
-    import SwitchOnOff from "@/components/SwitchOnOff.vue";
 
 const rooms = ref([]);
 const route = useRoute();
